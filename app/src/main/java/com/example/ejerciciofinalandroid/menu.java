dc2ea9120df19e60e6445e3e2a576921a196b7c6
AlfredoManuelSalcedo <150013355+AlfredoManuelSalcedo@users.noreply.github.com>
2024-02-06 09:06:32 +0100

    Creacion del menu con pestañas y archivo de primer fragmento creado

--- app/src/main/java/com/example/ejerciciofinalandroid/menu.java
+++ app/src/main/java/com/example/ejerciciofinalandroid/menu.java
@@ -1,8 +1,13 @@
 package com.example.ejerciciofinalandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+
+import com.google.android.material.tabs.TabLayout;
 
 public class menu extends AppCompatActivity {
 
@@ -10,5 +15,42 @@ public class menu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+        TabLayout tabLayout =(TabLayout) findViewById(R.id.pestanias);
+        tabLayout.addTab(tabLayout.newTab().setText("Badminton"));
+        tabLayout.addTab(tabLayout.newTab().setText("Ajedrez"));
+        tabLayout.addTab(tabLayout.newTab().setText("Senderismo"));
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                Fragment fragment = null;
+                switch (tab.getPosition()){
+                    case 0:
+                        fragment = new FirstFragment();
+                        break;
+                    case 1:
+                        fragment = new SecondFragment();
+                        break;
+                    case 2:
+                        fragment = new ThirdFragment();
+                        break;
+                }
+                FragmentManager fm = getSupportFragmentManager();
+                FragmentTransaction ft = fm.beginTransaction();
+                ft.replace(R.id.frane, fragment);
+                ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
+                ft.commit();
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
     }
 }
